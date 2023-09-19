080b6e90623a3b220c13faa3767483a34188c200
Роман Гришечкин <69861536+kocmos2012@users.noreply.github.com>
2023-09-19 06:59:53 +0000

    State lesson

--- src/Alert.js
+++ src/Alert.js
@@ -1,14 +1,22 @@
 import { useState } from 'react';
-export function Alert({ type, heading, children, closable }) {
+export function Alert({ type, heading, children, closable, onClose}) {
 const [visible, setVisible] = useState(true);
+ if (!visible) {
+     return null;
+ }
+ const handleCloseClick = () => {
+    setVisible(false);
+    if (onClose) {
+        onClose();
+    }
+}
  return (
  <div>
  <div>
- ...
  <span>{heading}</span>
  </div>
  {closable && (
- <button aria-label="Close">
+ <button aria-label="Close" onClick={handleCloseClick}>
  <span role="img" aria-label="Close">
  ❌
  </span>
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
  return (
  <div className="App">
  <Alert type="information" heading="Success" 
- closable>
+ closable onClose={() => console.log("closed")}>
  Everything is really good!
  </Alert>
  </div>
